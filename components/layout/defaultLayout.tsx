5281dbdec1793c2e73600c627a5d64a8eda79671
Neko <neko@qwq.icu>
2022-04-13 17:20:43 +0800

    🎈 perf(defaultLayout): impl paper margin transistion

--- components/layout/defaultLayout.tsx
+++ components/layout/defaultLayout.tsx
@@ -1,5 +1,6 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useEffect, useState } from 'react';
 import { Box, Container, Paper, Theme, Typography } from '@mui/material';
+import { throttle } from 'throttle-debounce';
 
 export interface DefaultLayoutProps {
   headerTitle?: string | ReactElement;
@@ -7,6 +8,31 @@ export interface DefaultLayoutProps {
 }
 
 export const DefaultLayout: FC<DefaultLayoutProps> = (props) => {
+  const [paperMarginTop, setPaperMarginTop] = useState(2);
+
+  useEffect(() => {
+    const onScroll = throttle(10, () => {
+      console.log(
+        document.documentElement.clientHeight -
+          (props.headerHeight / 100) * window.innerHeight +
+          document.documentElement.scrollTop
+      );
+      setPaperMarginTop(() =>
+        Math.min(
+          6,
+          ((document.documentElement.clientHeight -
+            (props.headerHeight / 100) * window.innerHeight +
+            document.documentElement.scrollTop) /
+            500) *
+            6
+        )
+      );
+    });
+    onScroll();
+    document.addEventListener('scroll', onScroll);
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [props.headerHeight]);
+
   return (
     <Box>
       <Box
@@ -55,7 +81,7 @@ export const DefaultLayout: FC<DefaultLayoutProps> = (props) => {
           elevation={18}
           sx={{
             padding: '2rem',
-            marginTop: '-2rem',
+            marginTop: `-${paperMarginTop}rem`,
             minHeight: `${100 - props.headerHeight}vh`,
             borderRadius: '0.5rem',
           }}
