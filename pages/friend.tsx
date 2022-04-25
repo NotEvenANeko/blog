729f827d7d71467aeccccaab490b5981c0361490
Neko <neko@qwq.icu>
2022-04-25 20:29:38 +0800

    ✨ feat(friend): add tips in friend page

--- pages/friend.tsx
+++ pages/friend.tsx
@@ -4,89 +4,122 @@ import { Avatar, Grid, Box, Typography, type Theme } from '@mui/material';
 
 import { getFriends } from 'lib';
 import { NextPageWithLayout } from './_app';
+import { LinkContainer } from 'components/linkContainer';
 
 const FriendPage: NextPageWithLayout<
   InferGetStaticPropsType<typeof getStaticProps>
 > = ({ data }) => (
-  <Grid
-    container
-    spacing={2}
+  <Box
     sx={{
-      p: '2rem 7.5%',
+      padding: '2rem 7.5%',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
     }}
   >
-    {data.map((item, index) => (
-      <Grid
-        item
-        xs={12}
-        md={4}
-        key={index}
-        component="a"
-        href={item.link}
-        sx={{
-          cursor: 'pointer',
-          borderRadius: '0.3rem',
-          p: '1rem',
-          m: '1rem 0',
-          transition: (theme: Theme) =>
-            theme.transitions.create(['background-color'], {
-              duration: theme.transitions.duration.standard,
-            }),
-          '&:hover': {
-            backgroundColor: '#f8f9fa',
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        m: 0,
+      }}
+    >
+      {data.map((item, index) => (
+        <Grid
+          item
+          xs={12}
+          md={4}
+          key={index}
+          component="a"
+          href={item.link}
+          sx={{
+            cursor: 'pointer',
+            borderRadius: '0.3rem',
+            p: '1rem',
+            m: '1rem 0',
             transition: (theme: Theme) =>
               theme.transitions.create(['background-color'], {
                 duration: theme.transitions.duration.standard,
               }),
-          },
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
+            '&:hover': {
+              backgroundColor: '#f8f9fa',
+              transition: (theme: Theme) =>
+                theme.transitions.create(['background-color'], {
+                  duration: theme.transitions.duration.standard,
+                }),
+            },
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+            alt="avatar"
+            src={item.avatar}
+            sx={{
+              width: '3rem',
+              height: '3rem',
+              marginRight: '0.75rem',
+            }}
+          >
+            <AccountCircle />
+          </Avatar>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+              height: '100%',
+              flex: '1',
+              lineHeight: '1.5rem',
+            }}
+          >
+            <Typography
+              sx={{
+                fontWeight: 'bold',
+              }}
+            >
+              {item.name}
+            </Typography>
+            <Typography
+              sx={{
+                maxHeight: '2rem',
+                fontSize: '0.85rem',
+                color: (theme: Theme) => theme.palette.text.secondary,
+                overflow: 'hidden',
+              }}
+            >
+              {item.description ?? ''}
+            </Typography>
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+    <Typography
+      sx={{
+        color: (theme: Theme) => theme.palette.text.secondary,
+        fontSize: '1rem',
+        m: '0 auto',
+      }}
+    >
+      想交换友链？请看
+      <LinkContainer
+        sx={{
+          display: 'inline',
+          color: '#0366d6',
         }}
       >
-        <Avatar
-          alt="avatar"
-          src={item.avatar}
-          sx={{
-            width: '3rem',
-            height: '3rem',
-            marginRight: '0.75rem',
-          }}
+        <Typography
+          component="a"
+          href={`${process.env.NEXT_PUBLIC_GITHUB_REPO_LINK}`}
+          target="_blank"
         >
-          <AccountCircle />
-        </Avatar>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-start',
-            height: '100%',
-            flex: '1',
-            lineHeight: '1.5rem',
-          }}
-        >
-          <Typography
-            sx={{
-              fontWeight: 'bold',
-            }}
-          >
-            {item.name}
-          </Typography>
-          <Typography
-            sx={{
-              maxHeight: '2rem',
-              fontSize: '0.85rem',
-              color: (theme: Theme) => theme.palette.text.secondary,
-              overflow: 'hidden',
-            }}
-          >
-            {item.description ?? ''}
-          </Typography>
-        </Box>
-      </Grid>
-    ))}
-  </Grid>
+          此处
+        </Typography>
+      </LinkContainer>
+    </Typography>
+  </Box>
 );
 
 FriendPage.headerHeight = 70;
